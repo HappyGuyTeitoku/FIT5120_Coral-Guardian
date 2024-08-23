627c5677b4c9fe95f12a5ccb4585e56b690ac235
Leo Wong <twon0024@student.monash.edu>
2024-08-23 16:55:36 +1000

    Added test page to see how adding pages would work

--- application.py
+++ application.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route('/')
 def index():
     return 'Welcome to flask app'
+
+@application.route('testpage')
+def testpage():
+    return render_template('testpage.html')
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0',port=80)
